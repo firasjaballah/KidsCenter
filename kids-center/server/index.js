7876ba40e2120ced3f5eb16ce0cf7706e2eba7ca
naceurmossaab <naceurmossaab@gmail.com>
2021-12-16 20:04:01 +0100

    signup component

--- kids-center/server/index.js
+++ kids-center/server/index.js
@@ -2,18 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var userRouter = require('./routers/user.js');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/user');
+mongoose.connect('mongodb://localhost/user').then((data) => console.log("mongoose connectetd"));
 
 app.use(bodyParser.json());
+app.use(cors());
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use('/', userRouter);
 
-
-app.use('/user', userRouter);
-
-app.listen(8000, function() {
+app.listen(8000, function () {
   console.log('listening on port 8000');
 });
