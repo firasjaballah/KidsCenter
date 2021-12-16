f1a278d9d6e19b5e9b1975e3deb4abe97fe1c939
Firas Jaballah <jaballah.firas89@gmail.com>
2021-12-16 20:15:24 +0100

    event cards

--- kids-center/src/app/app.module.ts
+++ kids-center/src/app/app.module.ts
@@ -7,15 +7,14 @@ import { ResultComponent } from './result/result.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { ForgetComponent } from './components/forget/forget.component';
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EventsComponent } from './components/events/events.component';
 
-// import {} from '.angular/material'
-
-
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { SearchComponent } from './search/search.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -31,7 +30,6 @@ import {HttpClientModule} from '@angular/common/http'
     SigninComponent,
     ForgetComponent,
     EventsComponent,
-    
     ResultComponent,
     SearchComponent,
  
@@ -39,11 +37,16 @@ import {HttpClientModule} from '@angular/common/http'
   imports: [
     BrowserModule,
     AppRoutingModule,
-     BrowserAnimationsModule,
-     FontAwesomeModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatGridListModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- kids-center/src/app/components/events/events.component.ts
+++ kids-center/src/app/components/events/events.component.ts
@@ -11,10 +11,60 @@ export class EventsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  states:string[]=[
+    "Tunis","Ariana","Ben arous","Manouba","Sousse","Sfax","Gabes",
+  "Médenine","Mahdia","Béja","Bizerte","Gafsa","Jendouba","Kairouan","Kasserine","Kef",
+  "Monastir","Nabeul","Sidi Bouzid","Siliana","Tataouine","Tozeur","Zaghouan","Kébili"
+  ]
+  events=[
+    {
+      title:'food party',
+      image:'https://www.threehillssportspark.co.uk/wp-content/uploads/2019/01/kids-party.png',
+      date:'01/01/2022 at 06 pm',
+      location:'Kids Event House',
+      fee:'30$ per child',
+      Description:'A party for kids where they can play and enjoy foods and make new freinds',
+      contact:'99 999 999'
+    },
+    {
+      title:'food party',
+      image:'https://www.threehillssportspark.co.uk/wp-content/uploads/2019/01/kids-party.png',
+      date:'01/01/2022 at 06 pm',
+      location:'Kids Event House',
+      fee:'30$ per child',
+      Description:'A party for kids where they can play and enjoy foods and make new freinds',
+      contact:'99 999 999'
+    },{
+      title:'food party',
+      image:'https://www.threehillssportspark.co.uk/wp-content/uploads/2019/01/kids-party.png',
+      date:'01/01/2022 at 06 pm',
+      location:'Kids Event House',
+      fee:'30$ per child',
+      Description:'A party for kids where they can play and enjoy foods and make new freinds',
+      contact:'99 999 999'
+    },{
+      title:'food party',
+      image:'https://www.threehillssportspark.co.uk/wp-content/uploads/2019/01/kids-party.png',
+      date:'01/01/2022 at 06 pm',
+      location:'Kids Event House',
+      fee:'30$ per child',
+      Description:'A party for kids where they can play and enjoy foods and make new freinds',
+      contact:'99 999 999'
+    },{
+      title:'food party',
+      image:'https://www.threehillssportspark.co.uk/wp-content/uploads/2019/01/kids-party.png',
+      date:'01/01/2022 at 06 pm',
+      location:'Kids Event House',
+      fee:'30$ per child',
+      description:'A party for kids where they can play and enjoy foods and make new freinds',
+      contact:'99 999 999'
+    }
+  ]
   
-  state=''
-  onfilter(selectedValue:HTMLSelectElement){
-    this.state=selectedValue.value
+  selectedValue:string
+  selectedstate:string
+  onfilter(){
+    this.selectedstate=this.selectedValue
   }
 
 }
