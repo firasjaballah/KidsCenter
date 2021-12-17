2bc5f58c89a4c1ef83b906b514c6459339aaa5bc
naceurmossaab <naceurmossaab@gmail.com>
2021-12-17 10:29:57 +0100

    server 2.0

--- kids-center/src/app/components/signin/signin.component.ts
+++ kids-center/src/app/components/signin/signin.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  form  : FormGroup;
+  result: Object;
 
-  constructor() { }
+  constructor(
+    private formSignIn: FormBuilder,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formSignIn.group({
+      username : '',
+      password : ''
+    })
+  }
+
+  submit(): void {
+    console.log(this.form.getRawValue());
+    this.http.post('http://localhost:8000/auth/signin', this.form.getRawValue())
+      .subscribe({
+        next: Response => {
+          console.log(Response);
+          this.result = Response;
+        },
+        error: error   => console.log("error", error)
+      });
+    // this.http.get('localhost:8000/user')
+    //   .subscribe(next => console.log("user added ..."));
+
   }
 
 }
