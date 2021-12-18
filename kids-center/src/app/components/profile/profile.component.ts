158f08206f3ad0c4993ccd8ba8dc6b76707c091f
Saharboukari <saharboukari@gmail.com>
2021-12-18 09:14:00 +0100

    modifying profile and update profile

--- kids-center/src/app/components/profile/profile.component.ts
+++ kids-center/src/app/components/profile/profile.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+ FullName:string ="sahar";
+ title:string="fullstack developper";
+ description:string="services for customers of all sizes";
+ Age:string="50";
+ email:string="sahar@gmail.com";
+ adresse:string="ariana.tunis";
+ city :string="ariana"
+ phone:string="254682"
+ user_img:""
+ experience:string="10"
   constructor() { }
 
   ngOnInit(): void {
