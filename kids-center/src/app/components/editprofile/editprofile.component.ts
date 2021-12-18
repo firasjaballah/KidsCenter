4c4ee87ee33d7e7438ec8639b40dadc7ac516674
Saharboukari <saharboukari@gmail.com>
2021-12-18 23:47:46 +0100

    profile done

--- kids-center/src/app/components/editprofile/editprofile.component.ts
+++ kids-center/src/app/components/editprofile/editprofile.component.ts
@@ -2,27 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-editprofile',
   templateUrl: './editprofile.component.html',
   styleUrls: ['./editprofile.component.css']
 })
 export class EditprofileComponent implements OnInit {
+  user:any;
+  subscription:Subscription;
   form  : FormGroup;
   constructor(
     private formEditprofile: FormBuilder,
     private http      : HttpClient,
-    private route     : Router
+    private route     : Router,
+    private data :DataService
   ) { }
 
   ngOnInit(): void {
+    this.subscription = this.data.currentMessage.subscribe((message: any) => {
+      this.user = message;
+      console.log("app component ", this.user);
+    });
+
     this.form = this.formEditprofile.group({
-      FullName:"",
+      category:"",
+      fullname:"",
       title:"",
       description:"",
-      Age:"",
       email:"",
-      adresse:"",
+      adress:"",
       city :"",
       phone:"",
       user_img:"",
@@ -30,11 +40,12 @@ export class EditprofileComponent implements OnInit {
     })
   }
   submit(): void {
-    this.http.post('http://localhost:8000/profile/_Id/', this.form.getRawValue())
+   console.log('editprofile user',this.user)
+    this.http.put(`http://localhost:8000/editprofile/${this.user._id}`, this.form.getRawValue())
     .subscribe({
       next:Response => {
         console.log("Edited", Response);
-        this.route.navigateByUrl('/profile/:userId');
+        this.route.navigateByUrl('profile');
       },
       error:error   => console.log("error", error)
     });
--- kids-center/src/app/components/profile/profile.component.ts
+++ kids-center/src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
 
 
 @Component({
@@ -8,25 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-    // mydata:any="data"
- FullName:string ="sahar";
- title:string="fullstack developper";
- description:string="services for customers of all sizes";
- Age:string="50";
- email:string="sahar@gmail.com";
- adresse:string="ariana.tunis";
- city :string="ariana"
- phone:string="254682"
- user_img:""
- experience:string="10"
-  constructor(private http:HttpClient) { }
+  user:any;
+  subscription:Subscription
+  
+//  fullname:string ="sahar";
+//  title:string="fullstack developper";
+//  description:string="services for customers of all sizes";
+//  Age:string="50";
+//  email:string="sahar@gmail.com";
+//  adresse:string="ariana.tunis";
+//  city :string="ariana"
+//  phone:string="254682"
+//  user_img:""
+//  experience:string="10"
+  constructor(private http:HttpClient, private data:DataService) { }
   // getprofile(){
   //   return this.http.get()
   // }
   ngOnInit(): void {
+  
+    this.subscription = this.data.currentMessage.subscribe((message: any) => {
+      this.user = message;
+      console.log("app component ", this.user);
+    });
 
-    
   }
 
 }
